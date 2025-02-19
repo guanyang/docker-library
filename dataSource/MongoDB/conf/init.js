6f74439d263a6bd51be9e20cb74d5ceff3fbe086
guanyang <guanyangsunlight@163.com>
2025-02-19 14:50:14 +0800

    update datasource config

--- dataSource/MongoDB/conf/init.js
+++ dataSource/MongoDB/conf/init.js
@@ -4,10 +4,12 @@ db = db.getSiblingDB('demo');
 // 创建一个集合并插入一些初始数据
 db.createCollection('employees');
 db.employees.insertMany([
-    { name: 'John Doe', position: 'Manager' },
-    { name: 'Jane Smith', position: 'Developer' },
-    { name: 'Alice Brown', position: 'Designer' }
+    { name: "John", position: "Manager", salary: 5000, age: 45 },
+    { name: "Jane", position: "Developer", salary: 4000, age: 28 },
+    { name: "Alice", position: "Designer", salary: 3500, age: 32 },
+    { name: "Bob", position: "Developer", salary: 4200, age: 29 },
+    { name: "Charlie", position: "Manager", salary: 5500, age: 50 }
 ]);
 
 // 创建一个索引（可选）
-db.employees.createIndex({ name: 1 });
+db.employees.createIndex({ position: 1, salary: -1 })
